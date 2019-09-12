325493e3651170ecdb93d4c1dafd7cbd9c9ee74e
Tim <tim.unverzagt@googlemai.com>
2019-09-13 01:08:03 +0200

    Add: Seperated Preprocessing and upcoming I/O into a single module

--- PyCharm/main.py
+++ PyCharm/main.py
@@ -1,29 +1,6 @@
 import numpy as np
-from gensim.models import KeyedVectors
+import preprocessing as pre
 from nltk.corpus import reuters
-from nltk.probability import FreqDist
-
-
-def load_embedding():
-    # Load embedding vectors directly from the file
-    return KeyedVectors.load_word2vec_format('WordEmbeddings/GoogleNews-vectors-negative300.bin', binary=True)
-
-
-def embed_word(model, word):
-    if word in model:
-        return model[word]
-    else:
-        # print("Word is not contained in the Language Model and will be randomly initialized.")
-        new_representation = np.random.rand(300)
-        # model.add(word, new_representation)
-        return new_representation
-
-
-def embed_doc(model, doc):
-    embedded_words = []
-    for word in doc[0]:
-        embedded_words.append(embed_word(model, word))
-    return np.array(embedded_words)
 
 
 def corpus_stats(docs):
@@ -58,45 +35,6 @@ def corpus_stats(docs):
     return
 
 
-def embedding_stats(model, docs):
-    words = []
-    words_in_model = []
-    words_out_of_model = []
-    for doc in docs:
-        for word in doc[0]:
-            words.append(word)
-            if word in model:
-                words_in_model.append(word)
-            else:
-                words_out_of_model.append(word)
-
-    no_in = len(words_in_model)
-    no_out = len(words_out_of_model)
-    unique_in = len(set(words_in_model))
-    unique_out = len(set(words_out_of_model))
-
-    fdist_out = FreqDist(word.lower() for word in words_out_of_model)
-
-    print("")
-    print('-'*50)
-    print('-'*50)
-    print("Amount of words in the model: ", no_in)
-    print("Unique words in the model: ", unique_in)
-    print("")
-    print("Amount of words outside the model: ", no_out)
-    print("Unique words outside the model: ", unique_out)
-    print("")
-    print("Percentage of words outside the model: ", round(100 * no_out / len(words), 1))
-    print("Percentage of unique words outside the model: ", round(100 * unique_out / len(set(words)), 1))
-    print("")
-    print("Most common words outside the model:")
-    print(fdist_out.most_common(10))
-    print('-'*50)
-    print('-'*50)
-    print("")
-    return
-
-
 def load_corpus():
     test_docs = []
     train_docs = []
@@ -113,15 +51,18 @@ def load_corpus():
 
 
 def main():
-    # docs are tupels (word_tokenizing, sentence_tokenizing)
-    [train_docs, test_docs] = load_corpus()
+    # tokenized_docs are tupels (word_tokenizing, sentence_tokenizing)
+    [tokenized_train_docs, tokenized_test_docs] = load_corpus()
 
-    model = load_embedding()
+    embedding_dict = pre.prepare_embedding(tokenized_train_docs + tokenized_test_docs)
+    pre.embedding_stats(embedding_dict, tokenized_train_docs)
+    pre.embedding_stats(embedding_dict, tokenized_test_docs)
+
     # embedding_stats(model, train_doc_words)
-    print(np.shape(embed_doc(model, train_docs[0])))
+    # print(np.shape(embed_doc(model, tokenized_train_docs[0])))
 
-    # corpus_stats(train_docs)
-    # corpus_stats(test_docs)
+    # corpus_stats(tokenized_train_docs)
+    # corpus_stats(tokenized_test_docs)
 
 
 main()
---
+++ PyCharm/preprocessing.py
@@ -0,0 +1,82 @@
+import numpy as np
+from gensim.models import KeyedVectors
+from nltk.probability import FreqDist
+
+# TODO: Write I/O-methods (cPickle) for the padded embedding to avoid the workload every execution
+
+
+def load_base_embedding():
+    # Load embedding vectors directly from the file
+    return KeyedVectors.load_word2vec_format('WordEmbeddings/GoogleNews-vectors-negative300.bin', binary=True)
+
+
+def prepare_embedding(tokenized_docs):
+    # TODO: I could try out casting every word in the docs to .lower() before lookup in BKV
+    base_keyed_vectors = load_base_embedding()
+    padded_dict = {}
+    for word in base_keyed_vectors.vocab:
+        padded_dict[word] = base_keyed_vectors[word]
+    for doc in tokenized_docs:
+        for word in doc[0]:
+            if word not in padded_dict:
+                # Randomly initiate key if the word is unknown
+                # TODO: New keys might be initialized slightly less naive even without training
+                print(word)
+                padded_dict[word] = np.random.rand(300)
+
+    return padded_dict
+
+
+def embed_word(embedding_dict, word):
+    try:
+        return embedding_dict[word]
+    except KeyError:
+        print("The word you are trying is not in the embedding dictionary!")
+        print("Check if the embedding was correctly prepared/loaded!")
+
+
+def embed_doc(embedding_dict, tokenized_doc):
+    embedded_words = []
+    for word in tokenized_doc[0]:
+        embedded_words.append(embed_word(embedding_dict, word))
+    return np.array(embedded_words)
+
+
+def embedding_stats(embedding_dict, tokenized_docs):
+    words = []
+    words_in_model = []
+    words_out_of_model = []
+    for doc in tokenized_docs:
+        for word in doc[0]:
+            words.append(word)
+            if word in embedding_dict:
+                words_in_model.append(word)
+            else:
+                words_out_of_model.append(word)
+
+    no_in = len(words_in_model)
+    no_out = len(words_out_of_model)
+    unique_in = len(set(words_in_model))
+    unique_out = len(set(words_out_of_model))
+
+    fdist_out = FreqDist(word.lower() for word in words_out_of_model)
+
+    print("")
+    print('-'*50)
+    print('-'*50)
+    print("Amount of words in the model: ", no_in)
+    print("Unique words in the model: ", unique_in)
+    print("")
+    print("Amount of words outside the model: ", no_out)
+    print("Unique words outside the model: ", unique_out)
+    print("")
+    print("Percentage of words outside the model: ", round(100 * no_out / len(words), 1))
+    print("Percentage of unique words outside the model: ", round(100 * unique_out / len(set(words)), 1))
+    print("")
+    print("Most common words outside the model:")
+    print(fdist_out.most_common(10))
+    print('-'*50)
+    print('-'*50)
+    print("")
+    return
+
