8690a5d94a4eab4c381e27f404be718708a9894e
Tim <tim.unverzagt@googlemail.com>
2019-09-16 22:37:52 +0200

    Add: Brought the network to gpu yielding significant speed-up

--- PyCharm/main.py
+++ PyCharm/main.py
@@ -2,8 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 # General modules
 import numpy as np
-import tensorflow as tf
-import tensorflow.keras as tfk
+from pathlib import Path
 
 # Personal modules
 import preprocessing as prep
@@ -17,23 +16,28 @@ def main():
     # tokenized_docs are tupels (word_tokenizing, sentence_tokenizing)
     [tok_train_docs, tok_test_docs] = io.load_corpus_docs()
 
-    ## Use this code to setup your embedding for the first time
-    # emb_dict = pre.prepare_embedding(tok_train_docs + tok_test_docs)
-    # io.save_embedding(emb_dict)
-
-    ## If you already have an embedding load it with this function
-    emb_dict = io.load_embedding()
+    if Path("dictionary.p").exists():
+        print("Loading previous model...")
+        emb_dict = io.load_embedding()
+    else:
+        print("No previous model found!")
+        print("Beginning to construct new model with GoogleNews-Vectors as base...")
+        emb_dict = prep.prepare_embedding(tok_train_docs + tok_test_docs)
+        io.save_embedding(emb_dict)
 
     # Embed the docs before you you feed them to the network
+    print("Embedding documents...")
     emb_train_docs = prep.embed_docs(emb_dict, tok_train_docs)
     emb_test_docs = prep.embed_docs(emb_dict, tok_test_docs)
     (batched_train_words, batched_train_cats) = prep.batch_docs(emb_train_docs, target_doc_len=30)
     (batched_test_words, batched_test_cats) = prep.batch_docs(emb_test_docs, target_doc_len=30)
 
-    model = Network(doc_len=30)
+    print("Developing network...")
+    model = Network(target_doc_len=30)
 
+    print("Training network...")
     model.train(input_array=batched_train_words,
-                annotation_array=batched_train_cats)
+                label_array=batched_train_cats)
 
     test_loss, test_acc = model.model.evaluate(batched_test_words, batched_test_cats)
 
--- PyCharm/network.py
+++ PyCharm/network.py
@@ -8,13 +8,13 @@ import tensorflow.keras as tfk
 
 class CustomNetworkHandler:
 
-    def __init__(self, doc_len):
-        middle_size = np.round(np.sqrt(doc_len * 300 * 90))
+    def __init__(self, target_doc_len):
+        middle_size = np.round(np.sqrt(target_doc_len * 300 * 90))
         self.model = tfk.Sequential([
-            tfk.layers.Input(shape=(doc_len, 300)),
-            tfk.layers.Flatten(input_shape=(doc_len, 300)),
+            tfk.layers.Input(shape=(target_doc_len, 300)),
+            tfk.layers.Flatten(input_shape=(target_doc_len, 300)),
             # 300 = dimensionality of embedding
-            tfk.layers.Dense(300 * doc_len, activation=tf.nn.relu),
+            tfk.layers.Dense(300 * target_doc_len, activation=tf.nn.relu),
             # middle layer is chosen so the downscaling factor is constant
             tfk.layers.Dense(middle_size, activation=tf.nn.relu),
             # 90 = number of categories
@@ -25,10 +25,9 @@ class CustomNetworkHandler:
                            loss='binary_crossentropy',
                            metrics=['accuracy'])
 
-    def train(self, input_array, annotation_array):
-        print("GPU Available: ", tf.test.is_gpu_available())
+    def train(self, input_array, label_array):
         self.model.fit(input_array,
-                       annotation_array,
+                       label_array,
                        batch_size=32,
-                       epochs=3)
+                       epochs=5)
         return
