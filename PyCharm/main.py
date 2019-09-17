398ead62d213d52afa439a3ea7270b8a381f1443
Tim <tim.unverzagt@googlemail.com>
2019-09-17 11:23:48 +0200

    Add: Implemented a basic CNN

--- PyCharm/main.py
+++ PyCharm/main.py
@@ -17,7 +17,7 @@ def main():
     [tok_train_docs, tok_test_docs] = io.load_corpus_docs()
 
     if Path("dictionary.p").exists():
-        print("Loading previous model...")
+        print("Loading previous language model...")
         emb_dict = io.load_embedding()
     else:
         print("No previous model found!")
@@ -29,21 +29,29 @@ def main():
     print("Embedding documents...")
     emb_train_docs = prep.embed_docs(emb_dict, tok_train_docs)
     emb_test_docs = prep.embed_docs(emb_dict, tok_test_docs)
-    (batched_train_words, batched_train_cats) = prep.batch_docs(emb_train_docs, target_doc_len=30)
-    (batched_test_words, batched_test_cats) = prep.batch_docs(emb_test_docs, target_doc_len=30)
+    (batched_train_words, batched_train_cats) = prep.batch_docs(emb_train_docs, target_doc_len=100)
+    (batched_test_words, batched_test_cats) = prep.batch_docs(emb_test_docs, target_doc_len=100)
 
     print("Developing network...")
-    model = Network(target_doc_len=30)
-
+    model = Network(target_doc_len=100, model_name='CNN')
+    # Add a channel dimension for CNNs
+    batched_train_words = np.reshape(batched_train_words, np.shape(batched_train_words) + (1,))
+    batched_test_words = np.reshape(batched_test_words, np.shape(batched_test_words) + (1,))
+    
     print("Training network...")
     model.train(input_array=batched_train_words,
                 label_array=batched_train_cats)
-
-    test_loss, test_recall, test_accuracy = model.model.evaluate(batched_test_words, batched_test_cats)
+    test_loss, test_recall, test_precision = model.model.evaluate(batched_test_words, batched_test_cats)
 
     print("Loss: ", test_loss)
     print("Recall: ", test_recall)
-    print("Accuracy: ", test_accuracy)
+    print("Precision: ", test_precision)
+    if not (test_precision + test_recall) == 0:
+        f1_measure = 2 * (test_precision*test_recall) / (test_precision + test_recall)
+    else:
+        f1_measure = 0
+
+    print("F1: ", f1_measure)
 
     return
 
--- PyCharm/network.py
+++ PyCharm/network.py
@@ -8,26 +8,47 @@ import tensorflow.keras as tfk
 
 class CustomNetworkHandler:
 
-    def __init__(self, target_doc_len):
-        middle_size = np.round(np.sqrt(target_doc_len * 300 * 90))
-        self.model = tfk.Sequential([
-            tfk.layers.Input(shape=(target_doc_len, 300)),
-            tfk.layers.Flatten(input_shape=(target_doc_len, 300)),
-            # 300 = dimensionality of embedding
-            tfk.layers.Dense(300 * target_doc_len, activation=tf.nn.relu),
-            # middle layer is chosen so the downscaling factor is constant
-            tfk.layers.Dense(middle_size, activation=tf.nn.relu),
-            # 90 = number of categories
-            tfk.layers.Dense(90, activation=tf.nn.sigmoid)
-        ])
+    def __init__(self, target_doc_len, no_of_classes=90, model_name='sandbox'):
+
+        # Hack to prevent a specific error with cudNN
+        # https://github.com/tensorflow/tensorflow/issues/24828
+        for gpu in tf.config.experimental.list_physical_devices('GPU'):
+            tf.compat.v2.config.experimental.set_memory_growth(gpu, True)
+
+        if model_name == 'sandbox':
+            middle_size = np.round(np.sqrt(target_doc_len * 300 * no_of_classes))
+            self.model = tfk.Sequential([
+                tfk.layers.Input(shape=(target_doc_len, 300)),
+                tfk.layers.Flatten(input_shape=(target_doc_len, 300)),
+                # 300 = dimensionality of embedding
+                tfk.layers.Dense(300 * target_doc_len, activation=tf.nn.relu),
+                # middle layer is chosen so the downscaling factor is constant
+                tfk.layers.Dense(middle_size, activation=tf.nn.relu),
+                # no_of_classes = number of categories
+                tfk.layers.Dense(no_of_classes, activation=tf.nn.sigmoid)
+            ])
+        elif model_name == 'CNN':
+            kernel_width = 3
+            no_of_filters = target_doc_len - kernel_width
+            self.model = tfk.Sequential([
+                tfk.layers.Conv2D(filters=1, kernel_size=(kernel_width, 300),
+                                  activation='relu', input_shape=(target_doc_len, 300, 1)),
+                tfk.layers.Flatten(input_shape=(no_of_filters, 1, 1)),
+                tfk.layers.Dense(units=no_of_classes, input_shape=(no_of_filters,), activation=tf.nn.sigmoid)
+            ])
+        else:
+            print("CustomNetworkHandler does not handle a model type called: ", model_name)
+            print("Please one of the following names: 'sandbox, 'CNN'")
+
+        self.model.summary()
 
         self.model.compile(optimizer='adam',
                            loss='binary_crossentropy',
-                           metrics=[tfk.metrics.Recall(), 'accuracy'])
+                           metrics=[tfk.metrics.Recall(), tfk.metrics.Precision()])
 
     def train(self, input_array, label_array):
         self.model.fit(input_array,
                        label_array,
                        batch_size=32,
-                       epochs=5)
+                       epochs=20)
         return
