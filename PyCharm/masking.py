e4f444ed01a2d7ab35f64e8dbf10b44670b5cbf7
Tim <tim.unverzagt@googlemail.com>
2019-10-14 20:14:04 +0200

    Add: Finding weight cut-of of the weights

--- PyCharm/masking.py
+++ PyCharm/masking.py
@@ -9,6 +9,18 @@ import tensorflow.keras as tfk
 def mask_network():
     init_model = tfk.models.load_model('SavedModels/test-init')
     init_model.summary()
+    percentile = 0
+    flattened_weights = None
+    weights_are_initialized = False
+    for layer in init_model.layers:
+        if layer.weights:
+            if not weights_are_initialized:
+                flattened_weights = tf.reshape(layer.weights[0], [-1])
+            else:
+                flattened_weights = tf.concat(flattened_weights, tf.reshape(layer.weights[0], [-1]))
+    percentile = np.percentile(flattened_weights.numpy(), 90)
+    print(percentile)
+
     trained_model = tfk.models.load_model('SavedModels/test-trained')
     trained_model.summary()
     return
