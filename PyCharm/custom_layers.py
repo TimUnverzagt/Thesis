Upd: Fixed a few bugs in the framework and switched to "Categorical Cross Entropy" with "Softmax" as output

PyCharm/custom_layers.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 # General modules
 import tensorflow as tf
-from tensorflow.keras import layers
+from tensorflow.python.keras import layers as keras_layers
 from tensorflow.python.keras import constraints
 from tensorflow.python.keras import regularizers
 from tensorflow.python.keras import activations
@@ -20,15 +20,17 @@ from tensorflow.python.ops import sparse_ops
 from tensorflow.python.ops import standard_ops
 
 
-class MaskedDense(layers.Layer):
+class MaskedDense(keras_layers.Dense):
 
     def __init__(self,
                  units,
-                 activation=None,
-                 kernel=None,
+                 initialization_weights=None,
+                 initialization_bias=None,
                  mask=None,
+                 activation=None,
                  use_bias=True,
-                 bias=None,
+                 kernel_initializer='glorot_uniform',
+                 bias_initializer='zeros',
                  kernel_regularizer=None,
                  bias_regularizer=None,
                  activity_regularizer=None,
@@ -38,27 +40,31 @@ class MaskedDense(layers.Layer):
         if 'input_shape' not in kwargs and 'input_dim' in kwargs:
             kwargs['input_shape'] = (kwargs.pop('input_dim'),)
 
-        super(MaskedDense, self).__init__(activity_regularizer=regularizers.get(activity_regularizer), **kwargs)
+        super(MaskedDense, self).__init__(units,
+                                          activation=activation,
+                                          use_bias=use_bias,
+                                          kernel_initializer=kernel_initializer,
+                                          bias_initializer=bias_initializer,
+                                          kernel_regularizer=kernel_regularizer,
+                                          bias_regularizer=bias_regularizer,
+                                          activity_regularizer=activity_regularizer,
+                                          kernel_constraint=kernel_constraint,
+                                          bias_constraint=bias_constraint,
+                                          **kwargs)
 
-        self.units = int(units) if not isinstance(units, int) else units
-        self.activation = activations.get(activation)
-        self.kernel_regularizer = regularizers.get(kernel_regularizer)
-        self.bias_regularizer = regularizers.get(bias_regularizer)
-        self.kernel_constraint = constraints.get(kernel_constraint)
-        self.bias_constraint = constraints.get(bias_constraint)
-        if kernel is None:
+        if initialization_weights is None:
             # TODO: Raise real exception
             print("No weight kernel has been supplied for the custom MaskedDense-layer!")
             print("A Critical Error is imminent!")
         else:
-            self.kernel = kernel
+            self.init_kernel = initialization_weights
         if mask is None:
-                print("No mask has been supplied for the custom MaskedDense-layer!")
-                print("While the layer is still functional it's reduced to a poor version of tf.keras.layers.Dense")
+            print("No mask has been supplied for the custom MaskedDense-layer!")
+            print("While the layer is still functional it's reduced to a poor version of tf.keras.layers.Dense")
         else:
-            self.mask =tf.cast(mask, self.dtype)
+            self.mask = tf.cast(mask, self.dtype)
         self.use_bias = use_bias
-        if bias is None:
+        if initialization_bias is None:
             # TODO Raise real exception
             print("No bias has been supplied for the custom MaskedDense-layer!")
             if use_bias:
@@ -66,21 +72,15 @@ class MaskedDense(layers.Layer):
             else:
                 print("As the layer is not set to use biases no problem may occur, but you are inviting problems.")
         else:
-            self.bias = bias
+            self.init_bias = initialization_bias
 
     def build(self, input_shape):
-        dtype = dtypes.as_dtype(self.dtype or K.floatx())
-        if not (dtype.is_floating or dtype.is_complex):
-            raise TypeError('Unable to build `MaskedDense` layer with non-floating point '
-                            'dtype %s' % (dtype,))
-        input_shape = tensor_shape.TensorShape(input_shape)
-        if tensor_shape.dimension_value(input_shape[-1]) is None:
-            raise ValueError('The last dimension of the inputs to `MaskedDense` '
-                             'should be defined. Found `None`.')
-        last_dim = tensor_shape.dimension_value(input_shape[-1])
-        self.input_spec = InputSpec(min_ndim=2,
-                                    axes={-1: last_dim})
-        self.built = True
+        super(MaskedDense, self).build(input_shape)
+        if (self.init_kernel is not None) & (self.init_bias is not None):
+            self.set_weights((self.init_kernel, self.init_bias))
+        elif(self.init_kernel is not None) | (self.init_bias is not None):
+            print("An initial weights or biases have been supplied, but the matching counterpart is missing.")
+            print("As such the given initial values are ignored!")
 
     def call(self, inputs):
         # self.kernel = math_ops.mul(self.kernel, tf.dtypes.cast(self.mask, dtype=tf.float32))
@@ -88,6 +88,7 @@ class MaskedDense(layers.Layer):
         if rank > 2:
             # Broadcasting is required for the inputs.
             outputs = standard_ops.tensordot(inputs, standard_ops.mul(self.kernel, self.mask), [[rank - 1], [0]])
+            # outputs = standard_ops.tensordot(inputs, self.kernel, [[rank - 1], [0]])
             # Reshape the output back to the original ndim of the input.
             if not context.executing_eagerly():
                 shape = inputs.shape.as_list()
@@ -97,7 +98,6 @@ class MaskedDense(layers.Layer):
             inputs = math_ops.cast(inputs, self._compute_dtype)
             '''
             if K.is_sparse(inputs):
-                masked_kernel = sparse_ops.sparse_tensor_dense_mul
                 outputs = sparse_ops.sparse_tensor_dense_matmul(inputs, self.kernel)
             else:
                 outputs = gen_math_ops.mat_mul(inputs, self.kernel)
@@ -108,28 +108,4 @@ class MaskedDense(layers.Layer):
         if self.activation is not None:
             return self.activation(outputs)  # pylint: disable=not-callable
         return outputs
-
-    def compute_output_shape(self, input_shape):
-        input_shape = tensor_shape.TensorShape(input_shape)
-        input_shape = input_shape.with_rank_at_least(2)
-        if tensor_shape.dimension_value(input_shape[-1]) is None:
-            raise ValueError(
-                'The innermost dimension of input_shape must be defined, but saw: %s'
-                % input_shape)
-        return input_shape[:-1].concatenate(self.units)
-
-    def get_config(self):
-        config = {
-            'units': self.units,
-            'activation': activations.serialize(self.activation),
-            'use_bias': self.use_bias,
-            'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
-            'bias_regularizer': regularizers.serialize(self.bias_regularizer),
-            'activity_regularizer':
-                regularizers.serialize(self.activity_regularizer),
-            'kernel_constraint': constraints.serialize(self.kernel_constraint),
-            'bias_constraint': constraints.serialize(self.bias_constraint)
-        }
-        base_config = super(MaskedDense, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
 

PyCharm/experiments.py
@@ -1,4 +1,5 @@
 # General modules
+import copy
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras as tfk
@@ -13,7 +14,18 @@ from datasets import mnist
 def test_basic_network_of_the_paper(epochs):
     print("Developing feedforward network on MNIST...")
     dense_model_wrapper = NetworkWrapper(model_identifier='MNIST-Lenet-FC')
-    initial_model = dense_model_wrapper.model
+    # Read out the config for creation of the masked model
+    model_config = dense_model_wrapper.model.get_config()
+    # Copy original weights by value
+    initial_weights = []
+    initial_biases = []
+    for i in range(len(dense_model_wrapper.model.weights)):
+        if (i % 2) == 0:
+            initial_weights.append(tf.identity(dense_model_wrapper.model.weights[i]))
+        elif (i % 2) == 1:
+            initial_biases.append(tf.identity(dense_model_wrapper.model.weights[i]))
+        else:
+            print("Separation weights and biases failed!")
 
     print("Quantifying MNIST datapoints...")
     data_splits = mnist.quantify_datapoints()
@@ -29,7 +41,9 @@ def test_basic_network_of_the_paper(epochs):
 
     print("Developing a masked network with the initial weights...")
     masked_model = masking.mask_initial_model(trained_model=dense_model_wrapper.model,
-                                              initial_model=initial_model,
+                                              initial_weights=initial_weights,
+                                              initial_biases=initial_biases,
+                                              model_config=model_config,
                                               pruning_percentage=20)
     lottery_ticket_wrapper = NetworkWrapper(model_identifier='GivenModel',
                                             given_model=masked_model)

PyCharm/main.py
@@ -21,19 +21,18 @@ def main():
     for gpu in tf.config.experimental.list_physical_devices('GPU'):
         tf.compat.v2.config.experimental.set_memory_growth(gpu, True)
 
-    # (full_network_history, masked_network_history) = experiments.test_basic_network_of_the_paper(epochs=5000)
-    # (full_network_history, masked_network_history) = experiments.test_creation_of_masked_network(epochs=3)
+    (full_network_history_wrapper, masked_network_history_wrapper) = experiments.test_basic_network_of_the_paper(epochs=100)
 
-    # storage.save_experimental_history(full_network_history.history, name='full_training_with_validation')
-    # storage.save_experimental_history(masked_network_history.history, name='masked_training_with_validation')
+    # storage.save_experimental_history(full_network_history_wrapper.history, name='full_training_with_validation_0')
+    # storage.save_experimental_history(masked_network_history_wrapper.history, name='masked_training_with_validation_0')
 
-    folderpath = 'Lenet-FCN-BCE/1'
-    full_network_history = storage.load_experimental_history('full_training_with_validation', folder=folderpath)
-    masked_network_history = storage.load_experimental_history('masked_training_with_validation', folder=folderpath)
-    
-    visualization.plot_measure_comparision_over_training(full_network_history, 'Full Network',
-                                                         masked_network_history, 'Masked Network',
-                                                         'val_precision_1', 'val_precision_2')
+    # folderpath = 'Lenet-FCN-CCE/test'
+    # full_network_history_wrapper = storage.load_experimental_history('full_training_with_validation_0', folder=folderpath)
+    # masked_network_history_wrapper = storage.load_experimental_history('masked_training_with_validation_0', folder=folderpath)
+
+    visualization.plot_measure_comparision_over_training(full_network_history_wrapper.history, 'Full Network',
+                                                         masked_network_history_wrapper.history, 'Masked Network',
+                                                         'accuracy', 'accuracy')
 
     return
 

PyCharm/masking.py
@@ -7,38 +7,45 @@ import tensorflow.keras as tfk
 from custom_layers import MaskedDense
 
 
-def mask_initial_model(trained_model, initial_model, pruning_percentage=50):
+def mask_initial_model(trained_model, initial_weights, initial_biases, model_config, pruning_percentage):
     masks = _create_masks(trained_model, pruning_percentage)
-    # initial_model.summary()
-    model_config = initial_model.get_config()
 
     masked_model = tfk.Sequential()
-    for idx, layer in enumerate(initial_model.layers):
-        if model_config['layers'][idx]['class_name'] == 'Dense':
+    weight_idx = 0
+    for layer in iter(model_config['layers']):
+        if layer['class_name'] == 'Flatten':
+            replacement_layer = tfk.layers.Flatten(batch_input_shape=layer['config']['batch_input_shape'])
+            masked_model.add(replacement_layer)
+
+        elif layer['class_name'] == 'Dense':
             print("Replacing Dense-layer of the model with a custom MaskedDense-layer")
-            if model_config['layers'][idx]['config']['activation'] == 'relu':
+            if layer['config']['activation'] == 'relu':
                 # print("Recognized an relu-activation.")
                 old_activation = tf.nn.relu
-            elif model_config['layers'][idx]['config']['activation'] == 'sigmoid':
+            elif layer['config']['activation'] == 'sigmoid':
                 # print("Recognized an sigmoid-activation.")
                 old_activation = tf.nn.sigmoid
-            elif model_config['layers'][idx]['config']['activation'] == 'linear':
-                # print("Recognized an sigmoid-activation.")
+            elif layer['config']['activation'] == 'softmax':
+                # print("Recognized an softmax-activation.")
+                old_activation = tf.nn.softmax
+            elif layer['config']['activation'] == 'linear':
+                # print("Recognized an linear activation.")
                 old_activation = None
             else:
                 # TODO: Throw real exception
                 print("The activation of the given model is not recognized.")
                 print("No activation was chosen. This will likely result in a critical error!")
 
-            replacement_layer = MaskedDense(units=layer.output_shape[1],
+            replacement_layer = MaskedDense(units=layer['config']['units'],
                                             activation=old_activation,
-                                            kernel=layer.kernel,
-                                            mask=masks[idx],
-                                            bias=layer.bias
+                                            initialization_weights=initial_weights[weight_idx],
+                                            mask=masks[weight_idx],
+                                            initialization_bias=initial_biases[weight_idx]
                                             )
             masked_model.add(replacement_layer)
+            weight_idx += 1
         else:
-            masked_model.add(layer)
+            print("Layer not recognized. Fatal Error imminent!")
 
     masked_model.build()
     # masked_model.summary()
@@ -55,18 +62,22 @@ def _create_masks(trained_model, pruning_percentage):
             else:
                 flattened_weights = tf.concat(flattened_weights, tf.reshape(layer.weights[0], [-1]))
 
-    quantile = np.percentile(np.abs(flattened_weights.numpy()), pruning_percentage)
+    if pruning_percentage == 0:
+        quantile = 0
+    else:
+        quantile = np.percentile(np.abs(flattened_weights.numpy()), pruning_percentage)
     print("Weight of the threshold for masking: ", np.round(quantile, 4))
 
     # List of tuples containing the idx and mask for each layer with trainable weights
-    masks = {}
-    for idx, layer in enumerate(trained_model.layers):
+    masks = []
+    for layer in iter(trained_model.layers):
         if layer.weights:
             # Only mask the weight-kernel (weights[0]) not the biases (weights[1])
             mask = _magnitude_threshold_mask(layer.weights[0], quantile)
-            masks[idx] = mask
+            masks.append(mask)
     return masks
 
 
 def _magnitude_threshold_mask(values, threshold):
-    return tf.cast(tf.map_fn(lambda x: x >= threshold, values, bool), tf.int32)
+    # Does this work as intended with numeric errors?
+    return tf.cast(tf.map_fn(lambda x: abs(x) >= threshold, values, bool), tf.int32)

PyCharm/network.py
@@ -12,7 +12,7 @@ class CustomNetworkWrapper:
 
         # Base initialization
         optimizer = 'adam'
-        loss = 'binary_crossentropy',
+        loss = 'categorical_crossentropy',
         # TODO: This breaks the self.model.evaluate function atm. Fix it!
         metrics = [tfk.metrics.Accuracy(), tfk.metrics.Recall(), tfk.metrics.Precision()]
 
@@ -71,7 +71,7 @@ class CustomNetworkWrapper:
                 tfk.layers.Dense(units=100,
                                  activation='relu'),
                 tfk.layers.Dense(units=10,
-                                 activation=tf.nn.sigmoid)
+                                 activation=tf.nn.softmax)
             ])
             optimizer = tfk.optimizers.Adam(learning_rate=1.2*1e-03)
             # loss = tfk.losses.mean_squared_error